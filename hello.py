3f1cd13fce8b9de8677f1e2b67f717a3afef8df6
Sarthak-builds <sarthakshiroty04@gmail.com>
2026-03-16 20:55:03 +0530

    fix

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-import utils  # This will compile utils.py to utils.cpython-312.pyc in __pycache__
+# import utils  # This will compile utils.py to utils.cpython-312.pyc in __pycache__
 
-print(utils.multiply(2, 3))  # Output: 6
+# print(utils.multiply(2, 3))  # Output: 6
+def fibonacci_generator(limit):
+    a, b = 0, 1
+    while a < limit:
+        yield a  # Yield the next number
+        a, b = b, a + b  # Update for next iteration
+
+# Using the generator
+fib_gen = fibonacci_generator(10)
+
+print(fib_gen)  # Output: 0 1 1 2 3 5 8
+
+# Generators can be converted to lists if needed
+print(list(fibonacci_generator(10)))  # Output: [0, 1, 1, 2, 3, 5, 8]
